e79a1cea313351d5ddadea96c6b22c50dab8e191
Saad-khan-18 <saadmaniyarsaadmaniyar@gmail.com>
2024-06-26 14:04:31 +0530

    this is task of class wednesday [26-06-24]

--- DATABASE.sql
+++ DATABASE.sql
@@ -3,3 +3,25 @@ CREATE DATABASE JUNE_10;
 CREATE DATABASE STUDENT;
 
 CREATE DATABASE X_WORKZ;
+
+CREATE DATABASE project;
+
+use  project;
+
+CREATE TABLE Airport_details
+(id int,
+airport_name varchar(20),
+arrival varchar(20),
+departure varchar(20),
+price varchar(20));
+
+CREATE TABLE Cricket_teams(
+id varchar(20),
+team_name varchar(20),
+no_of_players varchar(20),
+captain_name varchar(20),
+coach_name varchar(20));
+
+show tables;
+show databases;
+select * from Cricket_teams;
